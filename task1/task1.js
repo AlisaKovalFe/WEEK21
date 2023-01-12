7747a298bc45cd9e7ec0a0ec89b260920ba01ff5
Alisa <alisakovalhome@mail.ru>
2023-01-12 19:50:15 +0200

    Add smth in task1

--- task1/task1.js
+++ task1/task1.js
@@ -2,13 +2,18 @@ function onSearch() {
     let getGif = document.querySelector('#getGif').value
     console.log(getGif);
 
-    fetch('https://api.giphy.com/v1/gifs/search?api_key=ILy7aNThR6MbeADiAl9PV4SlwkYtad2B&q=' + getGif + '&limit=5&offset=0&rating=g&lang=en')
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=ILy7aNThR6MbeADiAl9PV4SlwkYtad2B&q=' + getGif + '&limit=5&offset=0&rating=g&lang=en', {
+        // headers: {
+            
+        // }
+    })
     .then (response => response.json())
     .then (gif => {
         console.log(gif)
 
         let firstImage = document.querySelector('#firstImage')
-        firstImage.src = gif.data[2].url
+        firstImage.src = gif.data[0].user.avatar_url
+
 
     })
     .catch(err => console.log(err))
